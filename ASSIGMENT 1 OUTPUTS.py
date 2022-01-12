a8f61e249eba69f30592533cf7811c8c9da96aa7
VISHNUBANSAL21 <97608478+VISHNUBANSAL21@users.noreply.github.com>
2022-01-12 22:37:12 +0530

    Add files via upload

---
+++ ASSIGMENT 1 OUTPUTS.py
@@ -0,0 +1,38 @@
+Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Vishnu Bansal\Desktop\assigment 1 py\ASSIGMENT 1 INPUTS.py =
+ANSWER 1
+Enter first number: 90
+Enter second number: 50
+Enter third number: 10
+Avearge of numbers:  50.0
+.......
+ANSWER 2
+Enter the  gross income in dollar : 5000000
+Enter the number of dependents: 10
+Person Income Tax is 99200.0
+.......
+ANSWER 3
+Enter SID: 21104042
+Enter Student Name: VISHNU BANSAL
+Type M for male , F for female,U for others
+Enter Gender: M
+Enter Course Name: EE
+Enter CGPA: 10.0
+Student [21104042, 'VISHNU BANSAL', 'M', 'EE', 10.0]
+.......
+ANSWER 4
+Marks of 1st student: 85
+Marks of 2nd student: 98
+Marks of 3rd student: 95
+Marks of 4th student: 100
+Marks of 5th student: 99
+[85.0, 95.0, 98.0, 99.0, 100.0]
+.......
+ANSWER 5.A
+color ['Red', 'Green', 'White', 'Black', 'Yellow']
+.......
+ANSWER 5.B
+color ['Red', 'Green', 'White', 'purple', 'Yellow']
+>>> 
